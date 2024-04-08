e226b4af90ba5ce4f6ee314d3b88a7f882162c73
Guillermo Abellán <guillermoabellan.dev@gmail.com>
2024-04-08 10:11:41 +0200

    Añadir cambios gitignore

--- CitasMedicas/obj/Debug/net8.0/CitasMedicas.AssemblyInfo.cs
+++ CitasMedicas/obj/Debug/net8.0/CitasMedicas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CitasMedicas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+186bca00e6ce5db6cf5c947909b33d8875dedeed")]
 [assembly: System.Reflection.AssemblyProductAttribute("CitasMedicas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CitasMedicas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
